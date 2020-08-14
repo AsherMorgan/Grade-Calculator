1e990cb30fc863f7571210c64fae1e05cf8c5874
AsherMorgan <59518073+AsherMorgan@users.noreply.github.com>
2020-08-14 13:41:50 -0700

    Implement percentage, leter, and color properties.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function load() {
                 {
                     "pointsEarned": 10,
                     "pointsPossible": 10,
+                    "categoryIndex":0,
                 }
             ],
             categories: [
@@ -24,25 +25,133 @@ function load() {
                 this.assignments.push({
                     "pointsEarned": null,
                     "pointsPossible": null,
+                    "categoryIndex": 0,
                 });
-                update();
             },
             removeAssignment: function(index) {
-                this.assignments.splice(index, 1)
-                update();
+                this.assignments.splice(index, 1);
             },
             addCategory: function() {
                 this.categories.push({
                     "name": "New category",
                     "weight": 0,
                 });
-                update();
             },
             removeCategory: function(index) {
-                this.categories.splice(index, 1)
-                update();
+                // Correct assignment category indexes
+                for (assignment of this.assignments) {
+                    if (assignment.categoryIndex == index) {
+                        assignment.categoryIndex = 0;
+                    }
+                    else if (assignment.categoryIndex > index) {
+                        assignment.categoryIndex--;
+                    }
+                }
+
+                // Remove category
+                this.categories.splice(index, 1);
             }
         },
+        computed: {
+            percentage: function() {
+                // Get assignment point totals
+                let totalEarned = 0;
+                let totalPossible = 0;
+                for (assignment of this.assignments) {
+                    // Get assignment data
+                    let weight = this.categories[assignment.categoryIndex].weight
+                    let pointsEarned = assignment.pointsEarned;
+                    let pointsPossible = assignment.pointsPossible;
+
+                    // Add to point totals
+                    if (weight !== null && pointsEarned !== null && pointsPossible !== null) {
+                        totalEarned += weight * pointsEarned;
+                        totalPossible += weight * pointsPossible;
+                    }
+                }
+
+                // Calculate grade percentage
+                let gradePercentage = (totalEarned / totalPossible) * 100;
+
+                // Return grade percentage
+                return gradePercentage.toFixed(3);
+            },
+            letter: function() {
+                // Get grade percentage
+                gradePercentage = this.percentage;
+                
+                // Return letter
+                if (gradePercentage >= 97) {
+                    return "A+";
+                }
+                else if (gradePercentage >= 93) {
+                    return "A";
+                }
+                else if (gradePercentage >= 90) {
+                    return "A-";
+                }
+                else if (gradePercentage >= 87) {
+                    return "B+";
+                }
+                else if (gradePercentage >= 83) {
+                    return "B";
+                }
+                else if (gradePercentage >= 80) {
+                    return "B-";
+                }
+                else if (gradePercentage >= 77) {
+                    return "C+";
+                }
+                else if (gradePercentage >= 73) {
+                    return "C";
+                }
+                else if (gradePercentage >= 70) {
+                    return "C-";
+                }
+                else if (gradePercentage >= 67) {
+                    return "D+";
+                }
+                else if (gradePercentage >= 63) {
+                    return "D";
+                }
+                else if (gradePercentage >= 60) {
+                    return "D-";
+                }
+                else if (gradePercentage || gradePercentage === 0) {
+                    return "F";
+                }
+                else {
+                    return "";
+                }
+            },
+            color: function() {
+                // Get grade letter
+                letter = this.letter;
+
+                // Return color
+                switch (letter) {
+                    case "A+":
+                    case "A":
+                    case "A-":
+                        return "#87BD6C";
+                    case "B+":
+                    case "B":
+                    case "B-":
+                        return "#CFE7FF";
+                    case "C+":
+                    case "C":
+                    case "C-":
+                        return "#FFFF8D";
+                    case "D+":
+                    case "D":
+                    case "D-":
+                        return "#F9AC48";
+                    case "F":
+                    default:
+                        return "#EF3D3D";
+                }
+            }
+        }
     });
 }
 
@@ -85,106 +194,6 @@ function UpdateTheme(theme = null) {
 
     // Save theme
     localStorage.setItem("theme", theme);
-}
-
-
-
-// Updates the final grade
-function update() {
-    // Get assignment point totals
-    let totalEarned = 0;
-    let totalPossible = 0;
-    for (assignment of document.getElementsByClassName("assignment")) {
-        // Get assignment data
-        let weight = parseFloat(assignment.getElementsByClassName("selectCategory")[0].value);
-        let pointsEarned = parseFloat(assignment.getElementsByClassName("pointsEarned")[0].value);
-        let pointsPossible = parseFloat(assignment.getElementsByClassName("pointsPossible")[0].value);
-
-        // Add to point totals
-        if (!isNaN(weight) && !isNaN(pointsEarned) && !isNaN(pointsPossible)) {
-            totalEarned += weight * pointsEarned;
-            totalPossible += weight * pointsPossible;
-        }
-    }
-
-    // Clear color classes
-    gradeElement = document.getElementById("grade");
-    gradeElement.classList.remove("letterA");
-    gradeElement.classList.remove("letterB");
-    gradeElement.classList.remove("letterC");
-    gradeElement.classList.remove("letterD");
-    gradeElement.classList.remove("letterF");
-
-    // Calculate grade percentage
-    let gradePercentage = (totalEarned / totalPossible) * 100;
-
-    // Set grade percentage
-    if (isFinite(gradePercentage)) {
-        document.getElementById("gradePercentage").textContent = `${gradePercentage.toFixed(3)}%`;
-    }
-    else {
-        document.getElementById("gradePercentage").textContent = gradePercentage;
-    }
-
-    // Round grade percentage (only effects letter grades)
-    gradePercentage = Math.round(gradePercentage);
-
-    // Set letter and color
-    if (gradePercentage >= 97) {
-        gradeElement.classList.add("letterA");
-        document.getElementById("gradeLetter").textContent = "A+";
-    }
-    else if (gradePercentage >= 93) {
-        gradeElement.classList.add("letterA");
-        document.getElementById("gradeLetter").textContent = "A";
-    }
-    else if (gradePercentage >= 90) {
-        gradeElement.classList.add("letterA");
-        document.getElementById("gradeLetter").textContent = "A-";
-    }
-    else if (gradePercentage >= 87) {
-        gradeElement.classList.add("letterB");
-        document.getElementById("gradeLetter").textContent = "B+";
-    }
-    else if (gradePercentage >= 83) {
-        gradeElement.classList.add("letterB");
-        document.getElementById("gradeLetter").textContent = "B";
-    }
-    else if (gradePercentage >= 80) {
-        gradeElement.classList.add("letterB");
-        document.getElementById("gradeLetter").textContent = "B-";
-    }
-    else if (gradePercentage >= 77) {
-        gradeElement.classList.add("letterC");
-        document.getElementById("gradeLetter").textContent = "C+";
-    }
-    else if (gradePercentage >= 73) {
-        gradeElement.classList.add("letterC");
-        document.getElementById("gradeLetter").textContent = "C";
-    }
-    else if (gradePercentage >= 70) {
-        gradeElement.classList.add("letterC");
-        document.getElementById("gradeLetter").textContent = "C-";
-    }
-    else if (gradePercentage >= 67) {
-        gradeElement.classList.add("letterD");
-        document.getElementById("gradeLetter").textContent = "D+";
-    }
-    else if (gradePercentage >= 63) {
-        gradeElement.classList.add("letterD");
-        document.getElementById("gradeLetter").textContent = "D";
-    }
-    else if (gradePercentage >= 60) {
-        gradeElement.classList.add("letterD");
-        document.getElementById("gradeLetter").textContent = "D-";
-    }
-    else if (gradePercentage || gradePercentage === 0) {
-        gradeElement.classList.add("letterF");
-        document.getElementById("gradeLetter").textContent = "F";
-    }
-    else {
-        document.getElementById("gradeLetter").textContent = "";
-    }
 }
 
 
